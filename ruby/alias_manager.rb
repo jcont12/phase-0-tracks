9084a5f416342b8bb1d75b0c63c32afcf7e72581
Jorge A. Contreras-Loreto <jorgeacontrerasl@gmail.com>
2017-04-26 18:03:33 -0500

    added consonant changer method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,10 +3,14 @@ Write a method that takes a spy real name and does the following:
 swaps first and last name
 changes vowels to the next vowel
 changes consonants to next consonant.
+Take into account the upcase and downcases
+Once name has been changed Capitalize first letter of each word
 
 Final input Felicia Torres - Vussit Gimodoe
 =end
 
+#Change vowel to the next vowel
+=begin
 def vowel_changer(letter)
 	if letter == "u"
 		new_letter = "a"
@@ -19,3 +23,15 @@ end
 
 vowel_changer ("a")
 
+
+#Change consonant to the next consonant
+def consonant_changer(letter)
+	if letter == "z"
+		new_letter = "b"
+	else
+	consonant_index = "bcdfghjklmnpqrstvwxyz".index(letter)
+    new_letter ="bcdfghjklmnpqrstvwxyz"[consonant_index.next]
+	end
+	p new_letter
+end
+=end
