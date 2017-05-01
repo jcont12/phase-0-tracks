dcd4a1bfd156397b5234df7541f53d0752e84fef
Jorge A. Contreras-Loreto <jorgeacontrerasl@gmail.com>
2017-04-30 23:02:51 -0500

    finished array drills

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -7,27 +7,86 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # printing each item in the array separated by an asterisk
 # ----
 
+zombie_apocalypse_supplies.each do |supplies|
+ p supplies
+ p "*"
+end
+
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
 # ----
+
+#NOTE: Since it does not ask us to iterate..:
+zombie_apocalypse_supplies[0] = "batteries"
+zombie_apocalypse_supplies[1] = "binoculars"
+zombie_apocalypse_supplies[2] = "CB radio"
+zombie_apocalypse_supplies[3] = "compass"
+zombie_apocalypse_supplies[4] = "hatchet"
+zombie_apocalypse_supplies[5] = "rations"
+zombie_apocalypse_supplies[6] = "shotgun"
+zombie_apocalypse_supplies[7] = "water jug"
+
+p zombie_apocalypse_supplies
+
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
 # ----
 
+def search(array,item)
+	found = []
+	array.each do |supplies|
+		if supplies == item
+			found << "yes"
+		end
+	end
+	if found[0] == "yes"
+		puts "item in backpack"
+	else
+		puts "not in backpack"
+	end
+end
+
+
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
 # ----
+
+array = []
+
+zombie_apocalypse_supplies.each do |supply|
+	array << supply
+end
+
+zombie_apocalypse_supplies = []
+
+index = 0
+
+until index == 5
+
+zombie_apocalypse_supplies<< array[index] 
+index += 1
+end 
+
+p zombie_apocalypse_supplies
+
+
 
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
 # and their supplies below. You should get rid of any duplicate items.
 # Find the built-in method that helps you accomplish this in the Ruby
 # documentation for Arrays.
+
 other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
+
+zombie_apocalypse_supplies.concat(other_survivor_supplies)
+
+p zombie_apocalypse_supplies
+
 # ----
 
 # Hash Drills
