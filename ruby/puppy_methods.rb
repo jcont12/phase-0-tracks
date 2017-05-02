aa26c6454c1173c380e08b5a81993eaac090e158
Jorge A. Contreras-Loreto <jorgeacontrerasl@gmail.com>
2017-05-02 17:01:43 -0500

    Added first puppy methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,33 @@ class Puppy
     toy
   end
 
+  def speak(number)
+  	puts "woof" * number
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(number)
+  	dog_years = number / 7
+  	p dog_years
+  end
+
+  def stay(number)
+  seconds = 0
+  	until seconds == number
+  		puts seconds
+  		seconds += 1
+  	end
+  puts "*moves*"
+  end
 end
 
+Funky = Puppy.new
+
+Funky.fetch("ball")
+Funky.speak(3)
+Funky.roll_over
+Funky.dog_years(28)
+Funky.stay(5)
