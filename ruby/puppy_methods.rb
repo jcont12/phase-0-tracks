a9afdb0d3984f433dac2f7fceb6a04461bb4548a
Jorge A. Contreras-Loreto <jorgeacontrerasl@gmail.com>
2017-05-02 19:12:38 -0500

    Created Otter class, with loop and methods defined

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,5 @@
+=begin
+** PUPPY CLASS: Commented out to begin own class **
 class Puppy
 
   def fetch(toy)
@@ -40,3 +42,38 @@ Funky.speak(3)
 Funky.roll_over
 Funky.dog_years(28)
 Funky.stay(5)
+=end
+
+#** OTTER CLASS **
+#Create two instance methods and an initialize method
+class Otter
+	def be_cute
+		puts "cleans its face"
+	end
+
+	def play
+		puts "runs and rolls around"
+	end
+
+	def initialize
+		puts "Initializing new otter instance..."
+	end
+end
+
+
+instances = 1
+index = 0
+otter_array = []
+#Creation of loop until we have 50 instances, and put them in data structure
+while instances <= 50
+	otter_array << "otter" + instances.to_s
+	otter_array[index] = Otter.new #
+	index += 1
+	instances += 1
+end
+
+#For each otter instance do the two instance methods
+otter_array.each do |otter|
+  otter.be_cute
+  otter.play
+end 
