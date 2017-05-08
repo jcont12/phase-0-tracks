bf35b24aa4342523ed4a61a17e68a72cc9f50269
Jorge A. Contreras-Loreto <jorgeacontrerasl@gmail.com>
2017-05-07 22:11:40 -0500

    Created original file and spec file, added pseudocode

---
+++ ruby/game/game.rb
@@ -0,0 +1,28 @@
+
+
+#Create a game class
+
+#Define what the mystery word will be through input
+
+#Create a method that turns the word into "_ _ _ _ _ _ " format
+	#input word
+	#Split into spaces 
+	#Count number of spaces
+	# add _ for that number of spaces
+
+#Show the competitor the "_ _ _ _ _ _" version and let it know the amount of guesses he will have
+
+#Ask the user to input a letter
+
+#If letter is in the mystery word, update "_" into corresponding letter, otherwise don't... in both cases assign guesses one less try and give feedback
+
+#Let the user know how many guesses he has left
+
+#if word is completed, congratulate the user
+
+#if guess counter runs out and he didn't finish, taunt
+
+
+#WATCH  OUT:
+#Dont let user write two words
+#Dont let user repeat a letter 
