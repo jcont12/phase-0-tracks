3d4956106311297081b1b2e3c3bfef0abb2bdde8
Jorge A. Contreras-Loreto <jorgeacontrerasl@gmail.com>
2017-05-11 17:43:32 -0500

    Created an array to store words and joined them

--- js/explore.js
+++ js/explore.js
@@ -1,12 +1,16 @@
 
 //1.- Create a function on javascript with a parameter (word to reverse)
 //2.- The word we want to reverse should be put into a variable so we can call its index (word[i])
-//3.- Get the length of the word, and print each letter from last to first using index and add them
+//3.- Get the length of the word, and store each letter from last to first within a variable through its index
+//4.- Join the array to create the word
 
 function reverse(word) {
-	var index = word.length
-	console.log(index)
-	console.log(word[0])
+	var reversedWord = []
+	for (var index = word.length - 1; index > -1; index--) {
+		reversedWord.push(word[index])
+	}
+	console.log(reversedWord.join("",reversedWord))
 }
 
 reverse("Jorge")
+reverse(" Hello World")
