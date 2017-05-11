2bfc6f51477f94d3b2e5f50213aa427069155cdb
Jorge A. Contreras-Loreto <jorgeacontrerasl@gmail.com>
2017-05-11 16:57:42 -0500

    Created first function for java word reverse

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+
+//1.- Create a function on javascript with a parameter (word to reverse)
+//2.- The word we want to reverse should be put into a variable so we can call its index (word[i])
+//3.- Get the length of the word, and print each letter from last to first using index and add them
+
+function reverse(word) {
+	console.log(word)
+}
+
+reverse("Jorge")
