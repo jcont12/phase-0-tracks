cfe7d16969de44435a039dc0dea2f92ef693c283
Jorge A. Contreras-Loreto <jorgeacontrerasl@gmail.com>
2017-05-12 16:38:07 -0500

    Added conditional statement so it only works for same array lengths

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,14 +12,18 @@ colors.push("palomino");
 console.log(horseNames[0]);
 console.log(colors);
 
-// Use your two arrays to create an object. The keys of your object should be horse names, and the values should be colors.
 
+// Use your two arrays to create an object. The keys of your object should be horse names, and the values should be colors.
+// Write a conditional so that hash is only created if array lengths are equal.
+// Hash were objects will be inserted
 var horseList = {};
-// horseList[horseNames[0]] = colors[0];
-// console.log(horseList)
 
 for (var i = 0; i < horseNames.length; i++) {
-	horseList[horseNames[i]] = colors[i]
+	if (horseNames.length == colors.length) {
+		horseList[horseNames[i]] = colors[i]
+	}
+	else {
+	}
 }
 
 console.log(horseList)
