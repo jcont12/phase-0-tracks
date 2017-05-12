c61b17a23f689f7ff763c13473a46694be2afa9e
Jorge A. Contreras-Loreto <jorgeacontrerasl@gmail.com>
2017-05-12 16:28:40 -0500

    Created a new variable to insert arrays to objects as key and value

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,11 +1,21 @@
 // Declare two separate arrays
 
-var colors = ["Blue","Green","purple","black"]
+var colors = ["Blue","Green","purple","black"];
 
-var horseNames = ["Speedy","Nacho","Agave","Bravo"]
+var horseNames = ["Speedy","Nacho","Agave","Bravo"];
 
-horseNames.push("Horsy")
-colors.push("palomino")
 
-console.log(horseNames)
-console.log(colors)
+// Create code to add values to arrays
+horseNames.push("Horsy");
+colors.push("palomino");
+
+console.log(horseNames[0]);
+console.log(colors);
+
+// Use your two arrays to create an object. The keys of your object should be horse names, and the values should be colors.
+
+var horseList = {};
+horseList[horseNames[0]] = colors[0];
+
+console.log(horseList)
+
