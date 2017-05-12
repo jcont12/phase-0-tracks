100ad00ac985c0ec8d754b7d9d84f5b1f3f5364f
Jorge A. Contreras-Loreto <jorgeacontrerasl@gmail.com>
2017-05-12 16:19:40 -0500

    Added code that adds values to array

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,9 @@
 var colors = ["Blue","Green","purple","black"]
 
 var horseNames = ["Speedy","Nacho","Agave","Bravo"]
+
+horseNames.push("Horsy")
+colors.push("palomino")
+
+console.log(horseNames)
+console.log(colors)
