c16bfd5f1f5eafd3998ecd7ee504ac62b6c32a38
Jorge A. Contreras-Loreto <jorgeacontrerasl@gmail.com>
2017-05-15 01:28:05 -0500

    Able to iterate through objects and print key value pairs, working on comparing them

--- js/algos.js
+++ js/algos.js
@@ -29,22 +29,37 @@ longest(phrases)
 // (You'll keep adding to this file, so we suggest grouping functions at the top and testing at the bottom of the file.)
 
 //1.- Create a function that takes in two objects
-//2.- Iterate through objects1 
-//3.- 
-//4.-
-//5.- Since the first item in the array will be the longest length, call index 0 of array
+//2.- Iterate through objects1, so that every keyvalue pair is analyzed
+//3.- While iterating on objects1, when analizing the first keyvalue pair an iteration of object 2 keyvalue pairs must be done
+//4.- If any of the keyvalue pairs on objects 1 is equal to an objects2 value pair declare true
+
 
 var objects1 = {name: "Steven", age: 54}
 var objects2 = {name: "Tamir", age: 54}
 
-function compare(objects1, objects2)
-		for 
+console.log(objects1.length)
 
+function compare(objects1, objects2) {
+	for (var property in objects1) {
+		console.log(property + ":" + objects1[property])
+    }
+		for (var property in objects2) {
+		console.log(property + ":" + objects2[property])
+    }
+}
 
-//Write a function that takes an integer for length, and builds and returns an array of strings of the given length. 
+compare(objects1,objects2)
+
+//Write a function that takes an integer for length, and builds and returns an array of strings of the given length.
 //So if we ran your function with an argument of 3, we would get an array of 3 random words back.
 // The words should be of randomly varying length, with a minimum of 1 letter and a maximum of 10 letters.
-// (This involves a new trick, generating a random number, that you'll have to look up, but the solution on how to do so is relatively straightforward.)
+// (This involves a new trick, generating a random number, that you'll have to look up, but the solution on how to do so
+// is relatively straightforward.)
+
+//1.- Create a function with a parameter for an integer
+//2.- Create an empty array variable
+//3.- While a variable is smaller than the number argument, create a random word
+//4.- Limit the word to have a maximum of 10 characters
 
 
 //Add driver code that does the following 10 times: generates an array, prints the array, feeds the array to your "longest word" function, and prints the result.
